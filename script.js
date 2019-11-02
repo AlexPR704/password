c5cad0c2ee88dba89a75a81d0fb1555499cc92c3
Alexander Garcia <alexg97343@gmail.com>
2019-11-02 16:41:53 -0400

    readme needs to be done{

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
 // //generate random password
 var password = "";
-var lowerChar = "abcdefghijklmnopqrstuvwxyz";
-var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var specialChar = "!@#$%^&*()_+";
-var numbersChar = "1234567890";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var special = "!@#$%^&*()_+";
+var numbers = "1234567890";
 
 
 
 //making the questions
-var passwordLength = (prompt("How many characters will you like your password to be? Please provide range from 8-128."));
+var userChoice = prompt("How many characters will you like your password to be? Please provide range from 8-128.");
 
-if (passwordLength >= 8 && passwordLength <= 128) {
-    var upper = confirm("Would you like upper characters?");
-    if (upper === true) {
-        password += upperChar;
+if (userChoice >= 8 && userChoice <= 128) {
+    var upper1 = confirm("Would you like upper characters?");
+    if (upper1 === true) {
+        password += upper;
     }
     console.log(password)
 
 
-    var lower = confirm("Would you like lower characters?");
-    if (lower === true) {
-        password += lowerChar;
+    var lower1 = confirm("Would you like lower characters?");
+    if (lower1 === true) {
+        password += lower;
     }
     console.log(password)
 
 
-    var special = confirm("Would you like special characters?");
-    if (special === true) {
-        password += specialChar;
+    var special1 = confirm("Would you like special characters?");
+    if (special1 === true) {
+        password += special;
     }
     console.log(password)
 
 
-    var numbers = confirm("Would you like numbers?");
-    if (numbers === true) {
-        password += numbersChar;
+    var numbers1 = confirm("Would you like numbers?");
+    if (numbers1 === true) {
+        password += numbers;
     }
     console.log(password)
 
 }
 
 else {
-    alert("Must be between 8-128!!!");
+    confirm("Must be between 8-128!!!");
     document.location.reload()
 }
 
@@ -51,8 +51,8 @@ else {
 function generate() {
     //create for loop to choose password characters
     var final = "";
-    for (i = 1; i <= passwordLength; i++) {
-        final = final + passwordLength.charAt(Math.floor(Math.random() * math.floor(passwordLength.length - 1)))
+    for ( i = 1; i <= userChoice; i++) {
+        final += password.charAt(Math.floor(Math.random() * Math.floor(password.length)))
     }
     //add password to textbox/display area
     console.log(final)
@@ -87,34 +87,3 @@ function copyPassword() {
 
 
 
-// function generate() {
-//     //set password length/complexity
-//     let complexity = document.getElementById("generate").value;
-
-
-//     //possible password values
-//     let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
-//     var lowerChar = "abcdefghijklmnopqrstuvwxyz";
-//     var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//     var specialChar = "!@#$%^&*()_+";
-//     var numbersChar = "1234567890";
-//     var char = "";
-
-//     //create for loop to choose password characters
-//     for(var i = 0; i <= complexity; i++){
-//         password = char + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
-
-//     }
-//     //add password to textbox/display area
-//     document.getElementById("display").value = password;
-// }
-
-// //function to copy the password to the clipboard
-// function copyPassword(){
-
-//     document.getElementById("display").select();
-
-//     document.execCommand("Copy");
-
-//     alert("Password is copied to clipboard!!!");
-// }
